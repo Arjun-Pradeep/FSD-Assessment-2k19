250f0b6fe7054b270523ee589d0650a688bd49d9
Arjun-Pradeep <arjunpradeepm17@gmail.com>
2019-03-27 10:45:17 +0530

    revnum_function_updated

--- JS/revnum.js
+++ JS/revnum.js
@@ -2,10 +2,16 @@ var a=102;
 var rev=0;
 var rem=0;
 
-while(a>0){
-    rem=a%10;
-    rev=rev*10+rem;
-    a=parseInt(a/10);
+console.log(revrs(a,rev,rem));
+
+function revrs(m,n,o){
+
+    
+    while(m>0){
+        o=m%10;
+        n=n*10+o;
+        m=parseInt(m/10);
+    }
+    return n;
 }
 
-console.log(rev);
