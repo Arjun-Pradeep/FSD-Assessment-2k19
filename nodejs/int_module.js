628854b71ea7defb49ce0dc059d5fd096cb85149
Arjun-Pradeep <arjunpradeepm17@gmail.com>
2019-04-09 07:48:00 +0530

    nodejs_internal_module_OS_completed

---
+++ nodejs/int_module.js
@@ -0,0 +1,11 @@
+const myos=require('os');
+
+var a=myos.platform();
+var b=myos.release();
+var c=myos.tmpdir();
+var d=myos.totalmem();
+
+console.log("OS :",a);
+console.log("OS Release :",b);
+console.log("OS Default Dir :",c);
+console.log("OS Total Memory :",d);
