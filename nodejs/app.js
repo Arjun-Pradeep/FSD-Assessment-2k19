d46e53897e12559077d9dcd66054332be5c6cf3e
Arjun-Pradeep <arjunpradeepm17@gmail.com>
2019-04-09 20:50:21 +0530

    expressjs_regform_completed

---
+++ nodejs/app.js
@@ -0,0 +1,23 @@
+const exp=require('express');
+const ehbs=require('express-handlebars');
+
+var app=exp()
+
+app.engine('handlebars',ehbs({defaultLayout:'main'}));
+app.set('view engine','handlebars');
+app.use(exp.static('views/static'));
+app.use(exp.urlencoded())
+
+app.post('/getdata',(req,res)=>{
+    res.send(req.body);
+});
+
+app.get('/',(req,res)=>{
+    res.render("index");
+});
+
+app.get('/contact',(req,res)=>{
+    res.render("contact");
+})
+
+app.listen(3000);
